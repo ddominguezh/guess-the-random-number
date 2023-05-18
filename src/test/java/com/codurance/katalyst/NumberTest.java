de441bb1b18c8c1419efb381d66ce01997a415cc
Diego Domínguez Herreros <diegoalbert500@gmail.com>
2023-05-18 23:09:01 +0200

    add assert false

--- src/test/java/com/codurance/katalyst/NumberTest.java
+++ src/test/java/com/codurance/katalyst/NumberTest.java
@@ -1,6 +1,7 @@
 package com.codurance.katalyst;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
@@ -20,10 +21,12 @@ public class NumberTest {
     @Test
     public void is_higher_that(){
         assertTrue(Number.create(5).isHigherThat(Number.create(4)));
+        assertFalse(Number.create(4).isHigherThat(Number.create(5)));
     }
 
     @Test
     public void is_lower_that(){
         assertTrue(Number.create(5).isLowerThat(Number.create(10)));
+        assertFalse(Number.create(10).isLowerThat(Number.create(5)));
     }
 }
