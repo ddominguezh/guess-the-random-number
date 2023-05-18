30261b5126820d34e969939a58883b1fc725279b
Diego Domínguez Herreros <diegoalbert500@gmail.com>
2023-05-18 22:59:55 +0200

    create test is higher that

--- src/test/java/com/codurance/katalyst/NumberTest.java
+++ src/test/java/com/codurance/katalyst/NumberTest.java
@@ -1,6 +1,7 @@
 package com.codurance.katalyst;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
 
@@ -14,5 +15,10 @@ public class NumberTest {
     @Test
     public void equals_number(){
         assertEquals(Number.create(5), Number.create(5));
+    }
+
+    @Test
+    public void is_higher_that(){
+        assertTrue(Number.create(5).isHigherThat(Number.create(4)));
     }
 }
