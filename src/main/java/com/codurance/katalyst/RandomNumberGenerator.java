91e95348681ef206f5bfc4f5bd59bb3c90130325
Diego Domínguez Herreros <diegoalbert500@gmail.com>
2023-05-18 23:23:30 +0200

    use java.util.Random

--- src/main/java/com/codurance/katalyst/RandomNumberGenerator.java
+++ src/main/java/com/codurance/katalyst/RandomNumberGenerator.java
@@ -1,10 +1,12 @@
 package com.codurance.katalyst;
 
+import java.util.Random;
+
 public class RandomNumberGenerator implements NumberGenerator {
 
     @Override
     public Number execute() {
-        return Number.create(1);
+        return Number.create(new Random().nextInt(100));
     }
     
 }
