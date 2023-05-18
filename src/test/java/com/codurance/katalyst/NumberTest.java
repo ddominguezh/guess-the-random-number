c1f73b44567da2685339e9caa6452da6bbdef88e
Diego Domínguez Herreros <diegoalbert500@gmail.com>
2023-05-18 05:53:39 +0200

    create equals number test

--- src/test/java/com/codurance/katalyst/NumberTest.java
+++ src/test/java/com/codurance/katalyst/NumberTest.java
@@ -8,6 +8,11 @@ public class NumberTest {
     
     @Test
     public void create_number(){
-        assertEquals(5, Number.create(5));
+        assertEquals(5, Number.create(5).value());
+    }
+
+    @Test
+    public void equals_number(){
+        assertEquals(Number.create(5), Number.create(5));
     }
 }
