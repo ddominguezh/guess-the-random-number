7665264e7affa28b09530624fa13f6567dd4f5da
Diego Domínguez Herreros <diegoalbert500@gmail.com>
2023-05-18 23:14:38 +0200

    create test, generate random values

--- src/test/java/com/codurance/katalyst/RandomNumberGeneratorTest.java
+++ src/test/java/com/codurance/katalyst/RandomNumberGeneratorTest.java
@@ -1,5 +1,6 @@
 package com.codurance.katalyst;
 
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
@@ -10,5 +11,11 @@ public class RandomNumberGeneratorTest {
     public void generate_number_greater_than_zero(){
         RandomNumberGenerator generator = new RandomNumberGenerator();
         assertTrue(generator.execute().isHigherThat(Number.create(0)));
+    }
+
+    @Test
+    public void generate_diferent_random_values(){
+        RandomNumberGenerator generator = new RandomNumberGenerator();
+        assertNotEquals(generator.execute(), generator.execute());
     }
 }
