41ad6d8fa74cf1a854b4a3b0a5483962d98c9ab6
Diego Domínguez Herreros <diegoalbert500@gmail.com>
2023-05-19 00:02:58 +0200

    create console screen

---
+++ src/main/java/com/codurance/katalyst/ConsoleScreen.java
@@ -0,0 +1,35 @@
+package com.codurance.katalyst;
+
+public class ConsoleScreen implements Screen {
+
+    @Override
+    public int readNumber() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'readNumber'");
+    }
+
+    @Override
+    public void isLower() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'isLower'");
+    }
+
+    @Override
+    public void isHigher() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'isHigher'");
+    }
+
+    @Override
+    public void lower() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'lower'");
+    }
+
+    @Override
+    public void won() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'won'");
+    }
+    
+}
---
+++ src/test/java/com/codurance/katalyst/ConsoleScreenTest.java
@@ -0,0 +1,29 @@
+package com.codurance.katalyst;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class ConsoleScreenTest {
+    
+    private ByteArrayOutputStream outputStream;
+
+    @BeforeEach
+    public void setUp(){
+        this.outputStream = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(this.outputStream));
+    }
+
+    @Test
+    public void get_number(){
+        ByteArrayInputStream inputStrem = new ByteArrayInputStream("5".getBytes());
+        System.setIn(inputStrem);
+        ConsoleScreen screen = new ConsoleScreen();
+        assertEquals(5, screen.readNumber());
+    }
+}
