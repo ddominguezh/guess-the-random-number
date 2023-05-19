44b1c07ee2b885be0521bc7f6d1d4ddd6f5cd78a
Diego Domínguez Herreros <diegoalbert500@gmail.com>
2023-05-19 05:27:04 +0200

    create test the player wins on the first move

---
+++ src/test/java/com/codurance/katalyst/GuessingNumberGameTest.java
@@ -0,0 +1,26 @@
+package com.codurance.katalyst;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import org.junit.jupiter.api.Test;
+
+public class GuessingNumberGameTest {
+    
+    @Test
+    public void the_player_wins_on_the_first_move(){
+        Screen screenMock = mock(Screen.class);
+        when(screenMock.readNumber()).thenReturn(5);
+
+        NumberGenerator numberGeneratorMock = mock(NumberGenerator.class);
+        when(numberGeneratorMock.execute()).thenReturn(Number.create(5));
+
+        GuessingNumberGame game = new GuessingNumberGame(screenMock, numberGeneratorMock);
+        game.play();
+        
+        verify(screenMock, times(1)).start();
+        verify(screenMock, times(1)).won();
+    }
+}
