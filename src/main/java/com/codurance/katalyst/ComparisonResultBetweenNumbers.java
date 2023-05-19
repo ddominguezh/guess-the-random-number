f84efc1cef14a7b94599379e2576beb2d5c3af79
Diego Domínguez Herreros <diegoalbert500@gmail.com>
2023-05-19 19:16:49 +0200

    set messages by result

--- src/main/java/com/codurance/katalyst/ComparisonResultBetweenNumbers.java
+++ src/main/java/com/codurance/katalyst/ComparisonResultBetweenNumbers.java
@@ -1,7 +1,14 @@
 package com.codurance.katalyst;
 
 public enum ComparisonResultBetweenNumbers {
-    LOWER,
-    EQUALS,
-    HIGHER
+    LOWER("I play a %d. The number is lower."),
+    EQUALS("I play a %d."),
+    HIGHER("I play a %d. The number is higher.");
+    private String message;
+    ComparisonResultBetweenNumbers(String message){
+        this.message  = message;
+    }
+    public String message(){
+        return this.message;
+    }
 }
