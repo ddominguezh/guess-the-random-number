7ca0134b380d1df5c4d842bcbfca380da7cf8ac6
Diego Domínguez Herreros <diegoalbert500@gmail.com>
2023-05-19 18:38:46 +0200

    verity times method is equals

--- src/test/java/com/codurance/katalyst/GuessingNumberGameTest.java
+++ src/test/java/com/codurance/katalyst/GuessingNumberGameTest.java
@@ -22,6 +22,23 @@ public class GuessingNumberGameTest {
 
         verify(screenMock, times(1)).start();
         verify(screenMock, times(1)).readNumber();
+        verify(screenMock, times(1)).isEquals();
+        verify(screenMock, times(1)).won();
+    }
+
+    @Test
+    public void the_player_wins_not_on_the_first_intent(){
+        Screen screenMock = mock(Screen.class);
+        when(screenMock.readNumber()).thenReturn(5);
+
+        NumberGenerator numberGeneratorMock = mock(NumberGenerator.class);
+        when(numberGeneratorMock.execute()).thenReturn(Number.create(10), Number.create(1), Number.create(5));
+
+        GuessingNumberGame game = new GuessingNumberGame(screenMock, numberGeneratorMock);
+        game.play();
+
+        verify(screenMock, times(1)).start();
+        verify(screenMock, times(3)).readNumber();
         verify(screenMock, times(1)).won();
     }
 }
